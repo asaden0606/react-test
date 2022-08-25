f2ea39c85304fbd0cd5cb030d7b8095d6b461a43
asaden0606 <asaden0606@gmail.com>
2022-08-25 16:12:25 +0900

    npmの修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,29 +5,70 @@ import FKVPage from './fkv';
 import FooterPage from './footer';
 import InformationPage from './information';
 import SolutionPage from './solution';
+const ScrollOut = require('scroll-out');
+import * as styles from '../scss/styles.module.scss';
 
 
+interface IndexPageProps { }
 
-  
-interface IndexPageProps {}
-  
 export default class IndexPage extends React.Component<IndexPageProps, {}> {
+  componentDidMount() {
+    ScrollOut({
+      targets: styles.jsfade,
+      threshold: 0.5,
+      once: "true",
+      offsetY: 400
+    });
+    ScrollOut({
+      targets: styles.jsfadeup,		
+      threshold: 0.5,
+      once: "true",
+      offsetY: 400
+    });   
+    ScrollOut({
+      targets: styles.jsfadeleft,
+      threshold: 0.5,
+      once: "true",
+      offsetY: 400
+    });
+  
+    ScrollOut({
+      targets: styles.jsfadedown,
+      threshold: 0.5,
+      once: "true",
+      offsetY: 400
+    });   
+    ScrollOut({
+      targets: styles.jsfaderight,
+      threshold: 0.5,
+      once: "true",
+      offsetY: 400
+    });    
+
+
+    /*
+    $(styles.animation).on('inview',function(){	
+      console.log("inview");
+      const $this:any = $(this);
+      $this.textillate({ 
+        initialDelay: 0,
+        in: { effect: 'bounceInDown' } 
+      });			
+      $(this).css("opacity",1);	
+    })
+    */
+  }
+
+
   public render() {
     return (
       <div>
         <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-        <link rel="stylesheet" href="/lib/lightbox2/css/lightbox.min.css"/>
-        <link rel="stylesheet" href="/lib/textillate/animate.css"/>
+        <link rel="stylesheet" href="/lib/lightbox2/css/lightbox.min.css" />
+        <link rel="stylesheet" href="/lib/textillate/animate.css" />
 
 
-        <script type="text/javascript" src="/lib/jquery/jquery.js"></script>
-        <script src="https://unpkg.com/scroll-out/dist/scroll-out.min.js"></script>
-        <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
         <script type="text/javascript" src="/lib/three/three.js"></script>
-        <script type="text/javascript" src="/lib/textillate/jquery.lettering.js"></script>
-        <script type="text/javascript" src="/lib/textillate/jquery.fittext.js"></script>
-        <script type="text/javascript" src="/lib/textillate/jquery.textillate.js"></script>
-        <script type="text/javascript" src="/lib/inview/jquery.inview.min.js"></script>
         <script type="text/javascript" src="/js/common.js"></script>
         <FKVPage></FKVPage>
         <SolutionPage></SolutionPage>
@@ -35,7 +76,7 @@ export default class IndexPage extends React.Component<IndexPageProps, {}> {
         <InformationPage></InformationPage>
         <ContactPage></ContactPage>
         <FooterPage></FooterPage>
-      </div>  
+      </div>
     )
   }
 }
